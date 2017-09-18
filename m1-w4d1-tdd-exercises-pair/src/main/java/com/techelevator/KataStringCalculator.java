0ff9be5f62dd6cc5057f19a5ff5f3129c5fda6bf
Ken Peterson <kenpeterson3@gmail.com>
2017-09-18 15:46:35 -0400

    w4d1 Team 7 final commit

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -5,13 +5,7 @@ import java.util.List;
 
 public class KataStringCalculator {
 	
-//	public static void main(String[] args){
-//		String[] temp=("").split(",");
-//		//int[] a=output("");
-//		System.out.println(temp.length);
-//	}
-	
-	public static int add(int[] output) {
+	public int add(int[] output) {
 
 		int x = 0;
 
@@ -23,7 +17,7 @@ public class KataStringCalculator {
 	}
 
 
-	public static int[] output(String numbers) { 
+	public int[] output(String numbers) { 
 		int[] output;
 		if (numbers.startsWith("//")) {
 			
@@ -54,40 +48,3 @@ public class KataStringCalculator {
 	
 }
 	
-	//public static void main(String[] args) {}
-	
-//		public int add(String numbers) {
-//		int total;
-//			if (numbers.isEmpty()) {
-//				total = 0;
-//				return total;
-//				
-//			} else {
-//				
-//				return ;
-//			}
-//		
-//		}
-//		
-//		public String[] splitOurString(String numbers) {
-//			
-//			List<Integer> processString = new ArrayList<>();
-//			
-//			for (int i = 0; i < numbers.length(); i++) {
-//				String[] output = numbers.split(",");
-//				
-//			return output;	
-//			}
-//			
-//		public int parseOutput(String output)	{			
-//			
-//			
-//			
-//		}
-//			
-//		}
-//		
-		
-		
-		
-
