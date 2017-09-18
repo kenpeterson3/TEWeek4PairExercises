3cfa1a92d6bc81a1d2a3141765ccfabe58c43934
Ken Peterson <kenpeterson3@gmail.com>
2017-09-18 15:19:35 -0400

    w4d1 pair exercises

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,5 +1,93 @@
 package com.techelevator;
 
-public class KataStringCalculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class KataStringCalculator {
+	
+//	public static void main(String[] args){
+//		String[] temp=("").split(",");
+//		//int[] a=output("");
+//		System.out.println(temp.length);
+//	}
+	
+	public static int add(int[] output) {
+
+		int x = 0;
+
+		for (int i = 0; i < output.length; i++) {
+
+			x += output[i];
+		}
+		return x;
+	}
+
+
+	public static int[] output(String numbers) { 
+		int[] output;
+		if (numbers.startsWith("//")) {
+			
+		String delimiter = numbers.substring(2,3);
+		String[] temp=numbers.split(delimiter);
+		output=new int[temp.length];
+		
+			if(temp[0]!=""){
+				for (int i = 0; i < temp.length ; i++) {
+					output[i] = Integer.parseInt(temp[i]);
+				}	
+			}
+			
+		} else {
+			
+		String[] temp=numbers.split(",|/n");
+		output=new int[temp.length];
+		
+			if(temp[0]!=""){
+				for (int i = 0; i < temp.length ; i++) {
+					output[i] = Integer.parseInt(temp[i]);
+				}	
+			}
+			
+		}
+		return output;
+	}	
+	
 }
+	
+	//public static void main(String[] args) {}
+	
+//		public int add(String numbers) {
+//		int total;
+//			if (numbers.isEmpty()) {
+//				total = 0;
+//				return total;
+//				
+//			} else {
+//				
+//				return ;
+//			}
+//		
+//		}
+//		
+//		public String[] splitOurString(String numbers) {
+//			
+//			List<Integer> processString = new ArrayList<>();
+//			
+//			for (int i = 0; i < numbers.length(); i++) {
+//				String[] output = numbers.split(",");
+//				
+//			return output;	
+//			}
+//			
+//		public int parseOutput(String output)	{			
+//			
+//			
+//			
+//		}
+//			
+//		}
+//		
+		
+		
+		
+
--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,46 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
+	
+	KataStringCalculator sut;
 
+	@Before
+	public void Setup() {
+		
+		sut = new KataStringCalculator();
+		
+	}	
+	@Test
+	public void testEmptyString() {
+		
+		int x=sut.add(sut.output(""));
+		assertEquals(0, x);
+					
+	}
+	@Test
+	public void testStringLengthOne() {
+		
+		int x=sut.add(sut.output("1"));
+		assertEquals(1, x);
+			
+	}
+	@Test
+	public void testStringLengthTwo() {
+		
+		int x=sut.add(sut.output("1,7"));
+		assertEquals(8, x);
+		
+	}
+	@Test
+	public void testStringLengthNine() {
+		
+		int x=sut.add(sut.output("1,7,12,4,8,20,78,0,420"));
+		assertEquals(550, x);
+	}
+	@Test
+	public void testStringLengthNewDelimter() {
+		int x=sut.add(sut.output("1/n7"));
+		assertEquals(8, x);
+	}
+	
 }
