72d36223f1b34a8d1cbfedf3456f4c4a2420601c
Richard Hresko <richardhresko@gmail.com>
2017-09-19 16:47:23 -0400

    wk4day2 pair exercises

--- m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
+++ m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
@@ -1,77 +1,39 @@
 package com.techelevator;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class WordCount {
 
 	public static void main(String[] args) {
+	
+		fileImporter();
 		
-		File inputFile = getInputFileFromUser();
-		String word = wordToSearch();
-		searchThingy(inputFile, word);
-		
-//		try (Scanner fileScanner = new Scanner(inputFile)) {
-//		//	while (fileScanner.hasNextLine()) {
-//				
-//				
-//			//}
-//		} catch (FileNotFoundException e) {
-//			System.out.println("Your file doesn't exist");
-//		}
-	}
-
-	private static File getInputFileFromUser() {
-		@SuppressWarnings("resource")
-		Scanner fileInput = new Scanner(System.in);
-		System.out.println("Please enter file path");
-		String path = fileInput.nextLine();
-
-		File inputFile = new File(path);
-		if (!inputFile.exists()) {
-			System.out.println(path + " Does not exist");
-			System.exit(1);
-
-		} else if (!inputFile.isFile()) {
-			System.out.println(path + " is not a file");
-			System.exit(1);
-
-		}
-		return inputFile;
-	}
-
-	public static String wordToSearch() {
-		@SuppressWarnings("resource")
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Please enter a word to search");
-		String searchWord = userInput.nextLine();
-
-		return searchWord;
 
 	}
-
-	public static String searchThingy(File inputFile, String searchWord) {
-
-		try (Scanner input = new Scanner(inputFile)) {
-			int x = 0;
-			while (input.hasNextLine()) {
-				
-				x++;
+	
+	public static void fileImporter() {
+		int wordCount = 0;
+		int sentenceCount = 0;
+		File alice = new File("/Users/richardhresko/workspace/team7-java-week-4-pair-exercises/m1-w4d2-file-io-part1-exercises-pair/alices_adventures_in_wonderland.txt");
+		try(Scanner input = new Scanner(alice)) {
+			while(input.hasNextLine()) {
 				String line = input.nextLine();
-				if (line.toLowerCase().contains(searchWord.toLowerCase())) {
-					System.out.println(x + ") " + line);
-				} else {
-					
+				if (! line.isEmpty()) {
+					String[] aliceArray = line.trim().split("\\s+");
+					wordCount += aliceArray.length;	
+					if(line.contains("!") || line.contains("?") || line.contains(".")) {
+						sentenceCount++;
+					}
 				}
-
+					
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Your file doesn't exist");
+		} catch(FileNotFoundException e) {
+			System.out.println("Your file does not exist");
 		}
-		return "";
+		System.out.println(wordCount);
+		System.out.println(sentenceCount);
+	
 	}
-
 }
